d562e71e1198b9fb2c4208e62ddbbb589c57a9f7
Crisvictor <xd67332@gmail.com>
2024-09-20 23:13:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@
 				context.textAlign = 'right';
 				context.textBaseline = 'bottom';
 				context.fillStyle = '#AAA';
-				context.fillText('按下 〔空白〕 開始 ',this.x,this.y);
+				context.fillText('Press space to start',this.x,this.y);
 			}
 		});
 		//事件绑定
